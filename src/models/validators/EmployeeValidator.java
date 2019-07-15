e58fc8775292efcd0b53724a3e97768d337922a1
jumpei.ezaki <gurikopurple@gmail.com>
2019-07-15 21:28:24 +0900

    Add employee create

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -38,7 +38,7 @@ public class EmployeeValidator {
 
         if(code_duplicate_check_flag){
             EntityManager em = DBUtil.createEntityManager();
-            long employees_count = (long)em.createNamedQuery("checkRegisterdCode",Long.class)
+            long employees_count = (long)em.createNamedQuery("checkRegisteredCode",Long.class)
                     .setParameter("code", code)
                     .getSingleResult();
             em.close();
